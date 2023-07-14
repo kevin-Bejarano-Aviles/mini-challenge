59acfd256d1eb5dd82244f7a0fa2ea67f9fd3ce5
kevin-Bejarano-Aviles <kevinbejarano2806@gmail.com>
2023-07-14 19:14:57 -0300

    challenge julio 1

---
+++ Julio/ChallengeJulio1.js
@@ -0,0 +1,12 @@
+/* 
+Dadas dos matrices no vacías de números enteros, escribe una función que determine si la segunda matriz es una subsecuencia de la primera.
+
+Una subsecuencia de una matriz es un conjunto de números que no necesariamente son adyacentes en la matriz, pero que están en el mismo orden en que aparecen en la matriz. Por ejemplo, los números [1, 3, 4] forman una subsecuencia de la matriz [1, 2, 3, 4], al igual que los números [2, 4]. Ten en cuenta que un solo número en una matriz y la matriz misma son ambas subsecuencias válidas de la matriz.
+
+Entrada de ejemplo:
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+Salida de ejemplo:
+true
+*/
